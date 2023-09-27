98a0e7f91984b97ccbf59105baae86a966d48d9c
Geekles <tondjilee@gmail.com>
2023-09-27 22:29:21 +0100

    deployment test

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import { fetchData } from '@/helpers';
-import { PER_PAGE, REVALIDATE } from '@/lib/constants';
 import dynamic from 'next/dynamic';
 
 const ContactUs = dynamic(
@@ -12,28 +10,11 @@ const AgencyName = dynamic(
   () => import('@/components/shared/atoms/agency-name')
 );
 const Socials = dynamic(() => import('@/components/home/organisms/socials'));
-const Gallery = dynamic(() => import('@/components/home/organisms/gallery'));
-
-const { DRIBBBLE_ACCESS_TOKEN } = process.env;
-
-async function getDribbblePosts() {
-  const posts: any[] = await fetchData(
-    `https://api.dribbble.com/v2/user/shots?access_token=${DRIBBBLE_ACCESS_TOKEN}&page=${1}&per_page=${PER_PAGE}`,
-    {
-      method: 'GET',
-      cache: 'force-cache',
-      next: {
-        revalidate: REVALIDATE,
-      },
-    }
-  );
-
-  return { posts };
-}
+const GalleryWrapper = dynamic(
+  () => import('@/components/home/templates/gallery-wrapper')
+);
 
 export default async function Home() {
-  const { posts } = await getDribbblePosts();
-
   return (
     <main className=''>
       <AgencyName />
@@ -41,10 +22,7 @@ export default async function Home() {
         className={'fixed bottom-8 left-8 z-[99] hidden gap-8 md:flex'}
       />
       <ContactUs />
-      <Gallery
-        posts={posts}
-        className={'absolute left-1/2 top-28 -translate-x-1/2'}
-      />
+      <GalleryWrapper />
       <ScrollTopButton />
     </main>
   );
---
+++ components/home/templates/gallery-wrapper.tsx
@@ -0,0 +1,37 @@
+import { fetchData } from '@/helpers';
+import { PER_PAGE, REVALIDATE } from '@/lib/constants';
+import Gallery from '@/components/home/organisms/gallery';
+
+type GalleryWrapperProps = {};
+
+const { DRIBBBLE_ACCESS_TOKEN } = process.env;
+
+async function getDribbblePosts() {
+  const posts: any[] = await fetchData(
+    `https://api.dribbble.com/v2/user/shots?access_token=${DRIBBBLE_ACCESS_TOKEN}&page=${1}&per_page=${PER_PAGE}`,
+    {
+      method: 'GET',
+      cache: 'force-cache',
+      next: {
+        revalidate: REVALIDATE,
+      },
+    }
+  );
+
+  return { posts };
+}
+
+const GalleryWrapper = async ({}: GalleryWrapperProps) => {
+  const { posts } = await getDribbblePosts();
+
+  return (
+    <>
+      <Gallery
+        posts={posts}
+        className={'absolute left-1/2 top-28 -translate-x-1/2'}
+      />
+    </>
+  );
+};
+
+export default GalleryWrapper;
