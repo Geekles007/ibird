c978eed3b5c9acf9be851e8905eaac2fae2c22c7
Geekles <tondjilee@gmail.com>
2023-09-28 00:18:00 +0100

    gallery deployment test

--- components/home/templates/gallery-wrapper.tsx
+++ components/home/templates/gallery-wrapper.tsx
@@ -22,7 +22,7 @@ async function getDribbblePosts() {
 }
 
 const GalleryWrapper = async ({}: GalleryWrapperProps) => {
-  // const { posts } = await getDribbblePosts();
+  const { posts } = await getDribbblePosts();
 
   return (
     <>
