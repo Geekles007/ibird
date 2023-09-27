c933a26de17e4a42b22e035906f114f74547e27f
Geekles <tondjilee@gmail.com>
2023-09-27 22:53:13 +0100

    deployment test

--- app/page.tsx
+++ app/page.tsx
@@ -19,14 +19,14 @@ export default async function Home() {
   return (
     <main className=''>
       <AgencyName />
-      {/*<Socials*/}
-      {/*  className={'fixed bottom-8 left-8 z-[99] hidden gap-8 md:flex'}*/}
-      {/*/>*/}
-      {/*<ContactUs />*/}
+      <Socials
+        className={'fixed bottom-8 left-8 z-[99] hidden gap-8 md:flex'}
+      />
+      <ContactUs />
       {/*<Suspense fallback={<>Loading</>}>*/}
       {/*  <GalleryWrapper />*/}
       {/*</Suspense>*/}
-      {/*<ScrollTopButton />*/}
+      <ScrollTopButton />
     </main>
   );
 }
